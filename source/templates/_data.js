b71af5b897f7a0bd987edecf49166aa8217fd0c2
lazy_slipper <avpontriagina@gmail.com>
2022-08-27 12:18:20 +0300

    Add map and video

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -18,6 +18,7 @@ export default {
 		"tablet": 768,
 		"mobile": null
 	},
+
 	"images": {
 		"map": {
 			"ext": "png",
@@ -25,6 +26,69 @@ export default {
 			"height": 454,
 			"maxdppx": 2,
 			"alt": "Магазин по адресу: улица Большая Конюшенная дом 19/8 офис 101, город Санкт-Петербург.",
+			"viewports": ["desktop", "tablet", "mobile"]
+		},
+
+		"intro": {
+			"ext": "jpg",
+			"width": 320,
+			"height": 373,
+			"maxdppx": 2,
+			"alt": "",
+			"viewports": ["desktop", "tablet", "mobile"]
+		},
+
+		"offer": {
+			"ext": "jpg",
+			"width": 260,
+			"height": 315,
+			"maxdppx": 2,
+			"alt": "",
+			"viewports": ["desktop", "tablet", "mobile"]
+		},
+
+		"review": {
+			"ext": "jpg",
+			"width": 350,
+			"height": 457,
+			"maxdppx": 2,
+			"alt": "",
+			"viewports": ["desktop", "tablet", "mobile"]
+		},
+
+		"product_1": {
+			"ext": "jpg",
+			"width": 260,
+			"height": 195,
+			"maxdppx": 2,
+			"alt": "Зайчик-попрыгайчик",
+			"viewports": ["desktop", "tablet", "mobile"]
+		},
+
+		"product_2": {
+			"ext": "jpg",
+			"width": 260,
+			"height": 195,
+			"maxdppx": 2,
+			"alt": "Корзинка для белья",
+			"viewports": ["desktop", "tablet", "mobile"]
+		},
+
+		"product_3": {
+			"ext": "jpg",
+			"width": 260,
+			"height": 195,
+			"maxdppx": 2,
+			"alt": "Большая корзинка для игрушек",
+			"viewports": ["desktop", "tablet", "mobile"]
+		},
+
+		"production-video": {
+			"ext": "jpg",
+			"width": 260,
+			"height": 150,
+			"maxdppx": 2,
+			"alt": "Процесс производства",
 			"viewports": ["desktop", "tablet", "mobile"]
 		}
 	}
