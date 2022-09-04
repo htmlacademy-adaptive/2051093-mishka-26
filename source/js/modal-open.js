ec4795ae9178fc5d1acfa4cd88fbd1e63d53f07d
lazy_slipper <avpontriagina@gmail.com>
2022-09-05 01:16:33 +0300

    Check Index-page with PP

---
+++ source/js/modal-open.js
@@ -0,0 +1,26 @@
+let offerButton = document.querySelector('.offer__button');
+let cardButtons = document.querySelectorAll('.product-card__button');
+let modal = document.querySelector('.modal');
+let closeButton = document.querySelector('.modal__close-button')
+
+if (offerButton) {
+	offerButton.addEventListener ('click', () => {
+		modal.classList.remove('modal--close');
+
+			closeButton.addEventListener ('click', () => {
+				modal.classList.add('modal--close');
+			})
+	})
+}
+
+if (cardButtons) {
+	cardButtons.forEach(element => {
+		element.addEventListener ('click', () => {
+			modal.classList.remove('modal--close');
+
+			closeButton.addEventListener ('click', () => {
+				modal.classList.add('modal--close');
+			})
+		})
+	});
+}
